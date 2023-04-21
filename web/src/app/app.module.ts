a2c2aa0653d5b37839a631a2d347d603f5db0f13
Gustavo dos Santos <gustavogugueira@gmail.com>
2023-04-20 23:18:16 -0300

    feat: Wheel component

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { WheelComponent } from './wheel/wheel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    WheelComponent
   ],
   imports: [
     BrowserModule,
